5c85ee2391d6ea4baa2e2022f0e6623d9481099f
Elbin12 <elbinshiby14@gmail.com>
2025-05-22 18:13:02 +0530

    created auth_connect, callback views for ghl authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('callback/', views.callback),
+    path('auth-connect/', views.auth_connect),
+    path('auth/tokens/', views.tokens),
+    path('ringcentral/access/', views.get_auth_from_jwt)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,177 @@
-from django.shortcuts import render
+import requests, base64
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.http import JsonResponse
 
 # Create your views here.
+
+GHL_CLIENT_ID = settings.GHL_CLIENT_ID
+GHL_REDIRECTED_URI = settings.GHL_REDIRECTED_URI
+BASE_URI = settings.BASE_URI
+GHL_CLIENT_SECRET = settings.GHL_CLIENT_SECRET
+TOKEN_URL = "https://services.leadconnectorhq.com/oauth/token"
+
+RINGCENTRAL_CLIENT_ID = settings.RINGCENTRAL_CLIENT_ID
+RINGCENTRAL_CLIENT_SECRET = settings.RINGCENTRAL_CLIENT_SECRET
+RINGCENTRAL_JWT = settings.RINGCENTRAL_JWT
+RINGCENTRAL_PHONE = settings.RINGCENTRAL_PHONE
+
+def auth_connect(request):
+    auth_url = ("https://marketplace.leadconnectorhq.com/oauth/chooselocation?response_type=code&"
+                f"redirect_uri={GHL_REDIRECTED_URI}&"
+                f"client_id={GHL_CLIENT_ID}&"
+                f"scope=conversations/message.readonly conversations/message.write"
+                )
+    return redirect(auth_url)
+
+def callback(request):
+    code = request.GET.get('code')
+
+    if not code:
+        return JsonResponse({"error": "Authorization code not received from OAuth"}, status=400)
+
+    return redirect(f'{BASE_URI}/auth/tokens?code={code}')
+
+def tokens(request):
+    authorization_code = request.GET.get("code")
+    if not authorization_code:
+        return JsonResponse({"error": "Authorization code not found"}, status=400)
+    
+    data = {
+        "grant_type": "authorization_code",
+        "client_id": GHL_CLIENT_ID,
+        "client_secret": GHL_CLIENT_SECRET,
+        "redirect_uri": GHL_REDIRECTED_URI,
+        "code": authorization_code,
+    }
+
+    response = requests.post(TOKEN_URL, data=data)
+    try:
+        response_data = response.json()
+        if not response_data.get('access_token'):
+            return render(request, 'onboard.html', context={
+                "message": "Invalid JSON response from API",
+                "status_code": response.status_code,
+                "response_text": response.text[:400]
+            }, status=400)
+        
+        # obj, created = GHLAuthCredentials.objects.update_or_create(
+        #     location_id= response_data.get("locationId"),
+        #     defaults={
+        #         "access_token": response_data.get("access_token"),
+        #         "refresh_token": response_data.get("refresh_token"),
+        #         "expires_in": response_data.get("expires_in"),
+        #         "scope": response_data.get("scope"),
+        #         "user_type": response_data.get("userType"),
+        #         "company_id": response_data.get("companyId"),
+        #         "user_id":response_data.get("userId"),
+
+        #     }
+        # )
+        # return render(request, 'onboard.html', context = {
+        #     "message": "Authentication successful",
+        #     "access_token": response_data.get('access_token'),
+        #     "token_stored": True
+        # })
+
+        print(response_data, 'response_data')
+
+    except requests.exceptions.JSONDecodeError:
+        return render(request, 'onboard.html', context={
+            "error": "Invalid JSON response from API",
+            "status_code": response.status_code,
+            "response_text": response.text[:500]
+        }, status=500)
+    
+
+
+
+
+
+def get_auth_from_jwt(request):
+
+    url = "https://platform.ringcentral.com/restapi/oauth/token"
+
+    client_credentials = f"{RINGCENTRAL_CLIENT_ID}:{RINGCENTRAL_CLIENT_SECRET}"
+    encoded_credentials = base64.b64encode(client_credentials.encode("utf-8")).decode("utf-8")
+
+    payload = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        "assertion": RINGCENTRAL_JWT
+    }
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Accept': "application/json",
+        'Authorization': f"Basic {encoded_credentials}"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    # print(response.status_code)
+    # print(response.json())
+    if response.status_code != 200:
+        return None
+    response_data = response.json()
+    print(response_data)
+    # response_data['ghl_location_id'] = location_id
+    # response_data['rc_phone_no'] = rc_phone_no
+    # response_data['jwt_code'] = jwt
+    # response_data['client_id'] = client_id
+    # response_data['client_secret'] = client_secret
+    return JsonResponse(response_data)
+
+
+def refresh_token(token):
+    url = "https://platform.ringcentral.com/restapi/oauth/token"
+
+    RC_CLIENT_ID = token.client_id
+    RC_CLIENT_SECRET = token.client_secret
+
+    client_credentials = f"{RC_CLIENT_ID}:{RC_CLIENT_SECRET}"
+    encoded_credentials = base64.b64encode(client_credentials.encode("utf-8")).decode("utf-8")
+
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": token.refresh_token
+    }
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Accept': "application/json",
+        'Authorization': f"Basic {encoded_credentials}"
+    }
+    ghl_location_id = token.ghl_location_id
+    rc_phone_no = token.rc_phone_no
+    jwt_code = token.jwt_code
+    response = requests.post(url, data=payload, headers=headers)
+    response_data = response.json()
+    print(response_data)
+    response_data['ghl_location_id'] = ghl_location_id
+    response_data['rc_phone_no'] = rc_phone_no
+    response_data['jwt_code'] = jwt_code
+    response_data['client_id'] = RC_CLIENT_ID
+    response_data['client_secret'] = RC_CLIENT_SECRET
+    # print(response_data)
+    return create_token(response_data)
+
+
+def create_token(data):
+    if not data.get('access_token'):
+        raise ValueError("Access token is required")
+    # # token, _ = RCToken.objects.update_or_create(
+    # #     owner_id = data.get('owner_id'),
+    # #     defaults={
+    # #         'access_token': data.get('access_token'),
+    # #         'token_type': data.get('token_type'),
+    # #         'expires_in': data.get('expires_in'),
+    # #         'refresh_token': data.get('refresh_token'),
+    # #         'refresh_token_expires_in': data.get('refresh_token_expires_in'),
+    # #         'scope': data.get('scope'),
+    # #         'ghl_location_id': data.get('ghl_location_id'),
+    # #         'rc_phone_no': data.get('rc_phone_no'),
+    # #         'jwt_code': data.get('jwt_code'),
+    # #         'client_id': data.get('client_id'),
+    # #         'client_secret': data.get('client_secret'),
+    # #         'created_at': timezone.now(),
+    # #     }
+    # )
+    return 'token'
